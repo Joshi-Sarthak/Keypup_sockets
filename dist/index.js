8a8d015c95bea90e2ef9fb9deb1bd7c0b7bb686a
Sarthak Joshi <sarthaksj02@gmail.com>
2025-01-05 18:47:56 +0530

    add basic  socket code

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,57 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const node_http_1 = require("node:http");
+const cors_1 = __importDefault(require("cors"));
+const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
+const server = (0, node_http_1.createServer)(app);
+app.use((0, cors_1.default)());
+const io = new socket_io_1.Server(server, {
+    cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+// Rooms mapping: { roomCode: [socketIds] }
+const rooms = {};
+io.on("connection", (socket) => {
+    console.log("User connected", { id: socket.id });
+    // Handle user joining a room
+    socket.on("join_room", (roomCode, name) => {
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = [];
+        }
+        // Check if the user's socket ID is already in the room
+        const userExists = rooms[roomCode].some((user) => user.id === socket.id);
+        if (!userExists) {
+            // Add the user's socket ID to the room
+            rooms[roomCode].push({ id: socket.id, name });
+            socket.join(roomCode);
+            console.log(`User ${socket.id} joined room ${roomCode}`);
+        }
+        else {
+            console.log(`User ${socket.id} is already in room ${roomCode}`);
+        }
+        console.log(rooms);
+        io.to(roomCode).emit("room_users", rooms);
+    });
+    // Handle user disconnection
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+        // Remove user from all rooms they were part of
+        for (const roomCode in rooms) {
+            rooms[roomCode] = rooms[roomCode].filter((user) => user.id !== socket.id);
+            // Notify other users in the room about the update
+            io.to(roomCode).emit("room_users", rooms[roomCode]);
+            // Clean up empty rooms
+            if (rooms[roomCode].length === 0) {
+                delete rooms[roomCode];
+                console.log(`Room ${roomCode} deleted (empty)`);
+            }
+        }
+    });
+});
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-app.listen(4000, () => {
+server.listen(4000, () => {
     console.log("Server is running on port 4000");
 });
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,75 @@
 import express from "express"
+import { createServer } from "node:http"
+import cors from "cors"
+import { Server } from "socket.io"
 
 const app = express()
+const server = createServer(app)
+app.use(cors())
+
+const io = new Server(server, {
+	cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+})
+
+interface User {
+	id: string
+	name: string
+}
+
+// Rooms mapping: { roomCode: [socketIds] }
+const rooms: Record<string, User[]> = {}
+
+io.on("connection", (socket) => {
+	console.log("User connected", { id: socket.id })
+
+	// Handle user joining a room
+	socket.on("join_room", (roomCode: string, name: string) => {
+		if (!rooms[roomCode]) {
+			rooms[roomCode] = []
+		}
+
+		// Check if the user's socket ID is already in the room
+		const userExists = rooms[roomCode].some((user) => user.id === socket.id)
+		if (!userExists) {
+			// Add the user's socket ID to the room
+			rooms[roomCode].push({ id: socket.id, name })
+			socket.join(roomCode)
+
+
+
+			console.log(`User ${socket.id} joined room ${roomCode}`)
+		} else {
+			console.log(`User ${socket.id} is already in room ${roomCode}`)
+		}
+
+		console.log(rooms)
+		io.to(roomCode).emit("room_users", rooms)
+	})
+
+	// Handle user disconnection
+	socket.on("disconnect", () => {
+		console.log("User disconnected:", socket.id)
+
+		// Remove user from all rooms they were part of
+		for (const roomCode in rooms) {
+			rooms[roomCode] = rooms[roomCode].filter((user) => user.id !== socket.id)
+
+			// Notify other users in the room about the update
+			io.to(roomCode).emit("room_users", rooms[roomCode])
+
+			// Clean up empty rooms
+			if (rooms[roomCode].length === 0) {
+				delete rooms[roomCode]
+				console.log(`Room ${roomCode} deleted (empty)`)
+			}
+		}
+	})
+})
 
 app.get("/", (req, res) => {
 	res.send("Hello World")
 })
 
-app.listen(4000, () => {
+server.listen(4000, () => {
 	console.log("Server is running on port 4000")
 })
