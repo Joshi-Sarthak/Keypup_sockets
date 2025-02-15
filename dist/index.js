348b237fd0574224f36719626e1f3e266df51d30
Sarthak Joshi <sarthaksj02@gmail.com>
2025-02-15 15:01:37 +0530

    change url

--- dist/index.js
+++ dist/index.js
@@ -16,10 +16,8 @@ const io = new socket_io_1.Server(server, {
 // Rooms mapping: { roomCode: Room }
 const rooms = {};
 io.on("connection", (socket) => {
-    console.log("User connected", { id: socket.id });
     // Handle user joining a room
     socket.on("create_room", (roomCode, name, email) => {
-        console.log("email : " + email);
         if (!rooms[roomCode]) {
             rooms[roomCode] = {
                 users: [],
@@ -41,7 +39,6 @@ io.on("connection", (socket) => {
                 rawChars: 0,
             });
             socket.join(roomCode);
-            console.log(`User ${socket.id} joined room ${roomCode}`);
         }
         else {
             console.log(`User ${socket.id} is already in room ${roomCode}`);
@@ -49,17 +46,14 @@ io.on("connection", (socket) => {
     });
     socket.on("check_room", (roomCode) => {
         if (rooms[roomCode]) {
-            console.log(`Room ${roomCode} exists`);
             socket.emit("room_exists", true);
         }
         else {
-            console.log(`Room ${roomCode} does not exist`);
             socket.emit("room_exists", false);
         }
     });
     socket.on("join_room", (roomCode, name, email) => {
         if (!rooms[roomCode]) {
-            console.log(`Room ${roomCode} does not exist`);
             return;
         }
         // Check if the user's socket ID is already in the room
@@ -76,27 +70,22 @@ io.on("connection", (socket) => {
                 rawChars: 0,
             });
             socket.join(roomCode);
-            console.log(`User ${socket.id} joined room ${roomCode}`);
         }
         else {
             console.log(`User ${socket.id} is already in room ${roomCode}`);
         }
-        console.log(rooms[roomCode]);
         io.to(roomCode).emit("room_users", rooms[roomCode].users);
         // Send the current mode and subtype to the new joiner
         socket.emit("changeNonHostMode", rooms[roomCode].mode, rooms[roomCode].subtype);
         socket.on("changeMode", (mode, selected) => {
-            console.log(mode, selected);
             rooms[roomCode].mode = mode;
             rooms[roomCode].subtype = selected;
             io.to(roomCode).emit("changeNonHostMode", mode, selected);
         });
         socket.on("startGame", (initialWords) => {
-            console.log(initialWords);
             io.to(roomCode).emit("startNonHostGame", initialWords);
         });
         socket.on("endGame", (mode, subtype, correctChars, rawChars, totalTime) => {
-            console.log(mode, subtype, correctChars, rawChars);
             if (rooms[roomCode]) {
                 // Update the player's stats
                 rooms[roomCode].users = rooms[roomCode].users.map((user) => user.id === socket.id
@@ -107,7 +96,6 @@ io.on("connection", (socket) => {
                         totalTime, finished: true }) : user);
                 // Check if all players have finished
                 const allFinished = rooms[roomCode].users.every((user) => user.finished);
-                console.log(allFinished);
                 if (allFinished) {
                     io.to(roomCode).emit("gameResults", rooms[roomCode].users);
                 }
@@ -116,7 +104,6 @@ io.on("connection", (socket) => {
     });
     // Handle user disconnection
     socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
         // Remove user from all rooms they were part of
         for (const roomCode in rooms) {
             rooms[roomCode].users = rooms[roomCode].users.filter((user) => user.id !== socket.id);
@@ -125,7 +112,6 @@ io.on("connection", (socket) => {
             // Clean up empty rooms
             if (rooms[roomCode].users.length === 0) {
                 delete rooms[roomCode];
-                console.log(`Room ${roomCode} deleted (empty)`);
             }
         }
     });
--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const server = createServer(app)
 app.use(cors())
 
 const io = new Server(server, {
-	cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+	cors: { origin: "https://keypup.vercel.app/", methods: ["GET", "POST"] },
 })
 
 interface User {
