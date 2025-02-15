d38d282912ce8e30c06fb1ce3accf52f2a0fa138
Sarthak Joshi <sarthaksj02@gmail.com>
2025-02-15 15:19:13 +0530

    slash lives matter

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const server = createServer(app)
 app.use(cors())
 
 const io = new Server(server, {
-	cors: { origin: "https://keypup.vercel.app/", methods: ["GET", "POST"] },
+	cors: { origin: "https://keypup.vercel.app", methods: ["GET", "POST"] },
 })
 
 interface User {
